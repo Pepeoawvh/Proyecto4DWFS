8142d0bf3b19942b7348b77e5e99dd01f8878eef
unknown <jpp.valdes@gmail.com>
2023-12-08 10:53:09 -0300

    prueba github

--- src/components/Pizzas.jsx
+++ src/components/Pizzas.jsx
@@ -9,7 +9,7 @@ import '../styles/Pizzas.css'
     <section className='cards'>
       <div className="pizza1">
 
-      <h2 className='TituloCard'>PIZZA PEPPERONI XBOX</h2> 
+      <h2 className='tituloCard'>PIZZA PEPPERONI XBOX</h2> 
       <div className='imagen1'/>
       <p>Ingredientes: Queso Mozarella, Salsa de Tomate y Pepperoni Americano</p>
       <h1 className='precio'>Precio: $9.990</h1>
@@ -18,7 +18,7 @@ import '../styles/Pizzas.css'
 
     <div className="pizza2">
 
-      <h2 className='TituloCard'>PIZZA VEGAN NINTENDO</h2>
+      <h2 className='tituloCard'>PIZZA VEGAN NINTENDO</h2>
       <div className='imagen2'/>
       <p>Ingredientes: Queso Mozarella, Salsa de Tomate, Choclo, Pimentón</p>
       <h1 className='precio'>Precio: $13.990</h1>
@@ -26,8 +26,8 @@ import '../styles/Pizzas.css'
     </div>
 
     <div className="pizza3">
-      <h2 className='TituloCard'>PIZZA ITALIAN PS4</h2>
-      <div className='imagen3'/>
+      <h2 className='tituloCard'>PIZZA ITALIAN PS4</h2>
+      <div className='imagen3 '/>
       <p>Ingredientes: Queso Mozarella, Salsa de Tomate, Jamón, Tomate</p>
       <h1 className='precio'>Precio: $12.990</h1>
       <button className='boton3'>Ver más</button>
