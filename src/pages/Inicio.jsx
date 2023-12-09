cdefee11985704342ba732079b8f8bf9a25dabd4
unknown <jpp.valdes@gmail.com>
2023-12-08 22:44:58 -0300

    critical

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,24 +2,18 @@
 
 import { Navbar } from '../components/Navbar'
 import "../styles/Inicio.css"
-<<<<<<< HEAD
-=======
 import { Landing } from '../components/Landing'
 import { Footer } from '../components/Footer'
->>>>>>> 835fa36da02458cb8aa40c2fb1072b27a74fc18e
 
  export const Inicio = () => {
   return (
     <>
     <div className="bannerJumbo">
     <Navbar />
-<<<<<<< HEAD
-=======
     <Landing/>
     <Footer />
     
 
->>>>>>> 835fa36da02458cb8aa40c2fb1072b27a74fc18e
     </div>
     </>
   )
