068dffaae77e4885e740972c2e312ab481c0cbfb
unknown <jpp.valdes@gmail.com>
2023-12-08 22:38:08 -0300

    borderraiusTest

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -2,16 +2,12 @@
 
 import { Navbar } from '../components/Navbar'
 import "../styles/Inicio.css"
-import { Pizzas } from '../components/Pizzas'
 
  export const Inicio = () => {
   return (
     <>
     <div className="bannerJumbo">
     <Navbar />
-    <Pizzas/>
-    
-
     </div>
     </>
   )
