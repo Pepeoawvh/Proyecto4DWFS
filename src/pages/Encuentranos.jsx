70573b61346cf102e97214807e59780a3c8b7c8a
Sebastián A. López <140351890+sebaao@users.noreply.github.com>
2023-12-17 00:57:55 -0300

    Update Encuentranos.jsx

--- src/pages/Encuentranos.jsx
+++ src/pages/Encuentranos.jsx
@@ -5,16 +5,22 @@ import "../styles/map.css";
 
 export const Encuentranos = () => {
   return (
-    <><div className="sobrenosotros">
+    <>
+     <div className="sobrenosotros">
      <Navbar />
       <div className="containermap">
         <div className="left-section">
-          <h2>Encuéntranos</h2>
-          <p>Información adicional</p>
-          <a href="https://api.whatsapp.com/send?phone=TUNUMERODEWHATSAPP" target="_blank" className="whatsapp-button">Contactar por WhatsApp</a>
-        </div>
+          <p className="titulo1x"> ENCUENTRANOS/> </p>
+
+          <p className="descripcionp1">• Encuéntranos en Av. La Plaza 13405, Las Condes</p>
+          <p className="descripcionp1">• Dudas contáctanos directamente por Whatsapp</p>
+          <button className="whatsapp-button">
+  <a href="https://api.whatsapp.com/send?phone=56975467484" target="_blank">
+    <i className="fab fa-whatsapp"></i> Contactar por WhatsApp
+  </a>
+</button>        </div>
         <div className="right-section">
-          <Map /> {/* Aquí deberías renderizar tu componente de Map */}
+          <Map />
         </div>
       </div>
       <Footer />
